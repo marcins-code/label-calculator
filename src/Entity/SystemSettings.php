256ee9d7edb9e45965957bd34ba961b21d730764
marcin <marcinpaczkowski@icloud.com>
2020-11-29 06:51:29 +0100

    SystemSettings entity init

---
+++ src/Entity/SystemSettings.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\SystemSettingsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=SystemSettingsRepository::class)
+ */
+class SystemSettings
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $setting;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $value;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSetting(): ?string
+    {
+        return $this->setting;
+    }
+
+    public function setSetting(string $setting): self
+    {
+        $this->setting = $setting;
+
+        return $this;
+    }
+
+    public function getValue(): ?int
+    {
+        return $this->value;
+    }
+
+    public function setValue(int $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+}
